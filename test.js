57bdb55085bae246a2eb794ad6e35fe62e968389
Chevelle <chevelle@list.ru>
2018-07-23 17:26:39 +0300

    1

--- test.js
+++ test.js
@@ -7,8 +7,48 @@ var ctx = canvas.getContext('2d');
 
 
 
-ctx.fillStyle = "rgb(200,0,0)";
+ctx.fillStyle = "rgb(135,206,235)";
 ctx.fillRect (10, 20, 70, 50);
 
-ctx.fillStyle = "rgb(0,200,0)";
-ctx.fillRect (30, 40, 50, 50);
+ctx.fillStyle = "rgb(135,106,300)";
+ctx.fillRect (30, 40, 70, 50);
+
+ctx.fillStyle = "rgb(255,105,180)";
+ctx.fillRect (50, 60, 70, 50);
+
+ctx.fillStyle = "rgb(135,206,235)";
+ctx.fillRect (715, 530, 70, 50);
+
+ctx.fillStyle = "rgb(135,106,300)";
+ctx.fillRect (695, 510, 70, 50);
+
+ctx.fillStyle = "rgb(255,105,180)";
+ctx.fillRect (675, 490, 70, 50);
+
+ctx.fillStyle = "rgb(135,206,235)";
+ctx.fillRect (10, 530, 70, 50);
+
+ctx.fillStyle = "rgb(135,106,300)";
+ctx.fillRect (30, 510, 70, 50);
+
+ctx.fillStyle = "rgb(255,105,180)";
+ctx.fillRect (50, 490, 70, 50);
+
+ctx.fillStyle = "rgb(135,206,235)";
+ctx.fillRect (715, 20, 70, 50);
+
+ctx.fillStyle = "rgb(135,106,300)";
+ctx.fillRect (695, 40, 70, 50);
+
+ctx.fillStyle = "rgb(255,105,180)";
+ctx.fillRect (675, 60, 70, 50);
+
+ctx.fillStyle = "rgb(255,105,180)";
+ctx.fillRect (0, 290, 800, 20);
+
+ctx.fillStyle = "rgb(135,206,235)";
+ctx.fillRect (390, 0, 20, 600);
+
+ctx.strokeStyle = "pink";
+ctx.strokeRect (390, 0, 20, 600);
+
